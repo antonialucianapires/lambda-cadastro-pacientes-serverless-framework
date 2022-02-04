3bdcecf23ca6811cbff60aa1e45c16f76c6e9234
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-02-04 00:53:32 -0300

    feat: endpoint para obter um paciente por id

--- handler.js
+++ handler.js
@@ -19,3 +19,25 @@ module.exports.listarPacientes = async (event) => {
     ),
   };
 };
+
+
+module.exports.obterPaciente = async (event) => {
+  console.log(event)
+  const { pacienteId } = event.pathParameters;
+
+  console.log(pacienteId)
+
+  const paciente = pacientes.find((paciente) => paciente.id === pacienteId);
+
+  if(!paciente) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({"error": "paciente não encontrado"}, null, 2),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(paciente, null, 2),
+  };
+};
