00b35d5c65b689dfce5d7ae38e27f840c348fccb
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-02-05 18:17:19 -0300

    feat: comunicação com dynamodb + carga de pacientes

--- handler.js
+++ handler.js
@@ -6,17 +6,41 @@ const pacientes = [
   { id: 3, nome: 'Jose', dataNascimento: '1998-06-06' }
 ]
 
+const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: 'PACIENTES',
+};
+
 module.exports.listarPacientes = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        pacientes
-      },
-      null,
-      2
-    ),
-  };
+  try {
+
+    let dados = await dynamoDB.scan(params).promise();
+
+    if (!dados || dados.Items.length === 0) {
+      return {
+        statusCode: 204,
+        body: JSON.stringify({})
+      }
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(dados)
+    }
+
+  } catch (error) {
+
+    console.log("Error", error);
+    return {
+      statusCode: error.statusCode ? error.statusCode : 500,
+      body: JSON.stringify({
+        error: error.name ? error.name : "Exception",
+        message: error.message ? error.message : "Unkown error"
+      })
+    }
+
+  }
 };
 
 
